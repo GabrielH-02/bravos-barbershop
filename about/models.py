77f04205e2366fe827790838b213d7df88a117f6
Gabriel Holmes <gabrielholmes2002@gmail.com>
2024-04-17 10:59:51 +0000

    adding meta classes

--- about/models.py
+++ about/models.py
@@ -11,6 +11,9 @@ class About(models.Model):
     updated_on = models.DateField(auto_now=True)
     content = models.TextField()
 
+    class Meta:
+        ordering = ['-updated_on']
+
     def __str__(self):
         return self.updated_on
 
@@ -20,6 +23,12 @@ class JobStatus(models.Model):
     """
     title = models.CharField(max_length=25, unique=True)
     qualif_level = models.IntegerField(unique=True)
+
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
 
 
 class Stylist(models.Model):
@@ -34,6 +43,9 @@ class Stylist(models.Model):
         JobStatus, on_delete=models.CASCADE, to_field='title'
     )
 
+    class Meta:
+        ordering = ['first_name']
+
     def __str__(self):
         return f"{self.stylist_id} is a {self.job_title}"
 
@@ -41,12 +53,16 @@ class HairRequest(models.Model):
     """
     Stores a single hair request message
     """
+    created_on = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=200)
     email = models.EmailField()
     phone_number = models.CharField(max_length=20)
     subject = models.CharField(max_length=200)
     message = models.TextField()
     read = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-created_on']
 
     def __str__(self):
         return f"Hair Request from {self.name}"
--- booking/models.py
+++ booking/models.py
@@ -12,12 +12,18 @@ class Service(models.Model):
         JobStatus, on_delete=models.CASCADE, to_field='qualif_level'
     )
 
+    class Meta:
+        ordering = ['title']
+
     def __str__(self):
         return f"Service: {self.title} at the price of {self.price}, only a barber with qualifcations of {self.level} can do it"
 
 
 class AppointmentTime(models.Model):
     title = models.TimeField(unique=True)
+
+    class Meta:
+        ordering = ['title']
 
     def __str__(self):
         return f"Appointment time: {self.title}"
@@ -37,6 +43,9 @@ class Appointment(models.Model):
     time_appo = models.ForeignKey(
         AppointmentTime, on_delete=models.CASCADE, related_name='appointmenttime'
     )
+
+    class Meta:
+        ordering = ['date_appo', 'time_appo']
 
     def __str__(self):
         return f"Appointment: {self.author} with {self.stylist} on {self.date_appo} at {self.time_appo}"
