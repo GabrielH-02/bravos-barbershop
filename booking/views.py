81a2f3c8a305fb9891f4719280e510c16f8d8f48
Gabriel Holmes <gabrielholmes2002@gmail.com>
2024-04-22 21:39:49 +0000

    adding links and adding LoginRequiredMixin

--- booking/views.py
+++ booking/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin  # Import the LoginRequiredMixin
 from .models import Service, Appointment
 
 
-class ServiceList(generic.ListView):
+class ServiceList(LoginRequiredMixin, generic.ListView):
     queryset = Service.objects.all().order_by('popular')
     template_name = "booking/services.html"
 
 
-class AppointmentList(generic.ListView):
+class AppointmentList(LoginRequiredMixin, generic.ListView):  # Add LoginRequiredMixin before generic.ListView
     queryset = Appointment.objects.all().order_by('date_appo')
     template_name = "booking/my-appointments.html"
